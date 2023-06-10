991f527ca0137b1ce200e1f2a1725d633f845171
aguilardc <aguilardc1105@gmail.com>
2023-06-09 22:06:59 -0500

    add play list body

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,8 @@ import {MediaPlayerComponent} from "@shared/components/media-player/media-player
 import {HeaderUserComponent} from "@shared/components/header-user/header-user.component";
 import { CardPlayerComponent } from './components/card-player/card-player.component';
 import { SectionGenericComponent } from './components/section-generic/section-generic.component';
+import { PlayListHeaderComponent } from './components/play-list-header/play-list-header.component';
+import { PlayListBodyComponent } from './components/play-list-body/play-list-body.component';
 
 
 
@@ -14,14 +16,18 @@ import { SectionGenericComponent } from './components/section-generic/section-ge
     MediaPlayerComponent,
     HeaderUserComponent,
     CardPlayerComponent,
-    SectionGenericComponent
+    SectionGenericComponent,
+    PlayListHeaderComponent,
+    PlayListBodyComponent
   ],
   exports: [
     SideBarComponent,
     MediaPlayerComponent,
     HeaderUserComponent,
     CardPlayerComponent,
-    SectionGenericComponent
+    SectionGenericComponent,
+    PlayListHeaderComponent,
+    PlayListBodyComponent
   ],
   imports: [
     CommonModule
